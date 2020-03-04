f07876b6014cfc3c1028359bd02755854eabb5fc
Dwi Noviandaru <dnoviandaru@gmail.com>
2020-03-04 13:07:10 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const User = (props) => {
+  return (
+    <div className='UserCard'>
+      <div className='UserCardTop'>
+        <img src={props.pic} />
+      </div>
+      <div className='UserCardBottom'>
+        <h3>{props.name}</h3>
+        <h5>{props.email}</h5>
+        <h5>{props.job}</h5>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="listOfCards">
+      <User name = 'Naruto Uzumaki'
+            email = 'naruto@konoha.gov'
+            pic = 'https://vignette.wikia.nocookie.net/naruto/images/4/42/Naruto_Part_III.png/revision/latest/scale-to-width-down/300?cb=20180117103539'
+            job = 'Project Manager'/>
+      <User name = 'Sasuke Uchiha'
+            email = 'sasuke@konoha.gov'
+            pic = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMrmfXsmvbR1rYFRFwX_2AEc7127qgeB86_eoLd7b9Mn0qfaUx'
+            job = 'Bussinest Analyst' />
+      <User name = 'Himawari Uzumaki'
+            email = 'himawari@konoha.gov'
+            pic = 'https://vignette.wikia.nocookie.net/naruto/images/2/26/Himawari.png/revision/latest/scale-to-width-down/340?cb=20171115100745'
+            job = 'FrontEnd Developer' />
     </div>
-  );
+  )
 }
 
 export default App;
