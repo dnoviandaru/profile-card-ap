8f474d51ff33a5b0b82b09ecb6ea812871739ae2
Dwi Noviandaru <dnoviandaru@gmail.com>
2020-03-31 23:01:41 +0700

    profile page

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import NavBar from '../menu/Menu';
-import Slideshow from '../menu/Slideshow';
+import Fade from 'react-reveal/Fade';
 
 class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            headerTagline: [//Line 1 For Header
+                'Hi 👋 You found me! I am Dwi Noviandaru.',
+                //Line 2 For Header
+                'Tennis Player ',
+                //Line 3 For Header
+                'from Yogyakarta'
+            ]
+        }
+    }
     render() {
         return (
             <div>
-                <NavBar></NavBar>
-                <Slideshow></Slideshow>
+                <NavBar selectedKeys="profile"></NavBar>
+                <Fade top>
+                    <p className='header-title'>
+                    {this.state.headerTagline[0]}<br></br>{this.state.headerTagline[1]}<br></br>
+                    {this.state.headerTagline[2]}
+                    <br></br></p>
+                </Fade>
             </div>
         );
     }
