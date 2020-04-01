feb610ed9054436b93bc3c2fcd4869e505fcb8bb
Dwi Noviandaru <dnoviandaru@gmail.com>
2020-04-01 22:23:10 +0700

    Updates

---
+++ precache-manifest.1ac0fb1fc0d3bda4b6f50e67c9463b53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a8d4eafa67fd26c4fac6c25662d0d5",
+    "url": "/profile-card-ap/index.html"
+  },
+  {
+    "revision": "cf03451bb13ec4d4dbaa",
+    "url": "/profile-card-ap/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "1298b5b54cafdb61c4d3",
+    "url": "/profile-card-ap/static/css/main.4d8b12e2.chunk.css"
+  },
+  {
+    "revision": "cf03451bb13ec4d4dbaa",
+    "url": "/profile-card-ap/static/js/2.617ca875.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/profile-card-ap/static/js/2.617ca875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1298b5b54cafdb61c4d3",
+    "url": "/profile-card-ap/static/js/main.c8679c62.chunk.js"
+  },
+  {
+    "revision": "9d754e7f0b2ace20f86a",
+    "url": "/profile-card-ap/static/js/runtime-main.ea9ae4d1.js"
+  }
+]);
